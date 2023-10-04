c2db02d63d667bbe6909eef897e26127bb74fdf8
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-03 23:03:33 -0500

    Update and rename albums-by-aritst.php to songs-in-album.php

---
+++ songs-in-album.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-songs-in-album.php");
+
+$pageTitle = "Songs in Album";
+include "view-header.php";
+$albums = selectSongsInAlbum($_POST['sid']);
+include "view-songs-in-album.php";
+include "view-footer.php"
+?>
