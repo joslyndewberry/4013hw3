4c255dda30ae5f9d25718c934bbe9e747ebd085e
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-03 23:12:23 -0500

    Update view-artists.php

--- view-artists.php
+++ view-artists.php
@@ -6,7 +6,6 @@
       <th>ID</th>
       <th>Name</th>
         <th></th>
-        <th></th>
       </tr>
     </thead>
     <tbody>
@@ -17,12 +16,6 @@ while ($artist = $artists->fetch_assoc()) {
     <td><?php echo $artist['artist_id']; ?></td>
     <td><?php echo $artist['artist_name']; ?></td>
     <td><a href="songs-by-artist.php?id=<?php echo $artist['artist_id']; ?>">Songs</a></td>
-    <td>
-      <form method="post" action="albums-by-artist.php">
-        <input type="hidden" name="alid" value="<?php echo $artist['artist_id']; ?>">
-        <button type="submit" class="btn btn-primary">Albums</button>
-      </form>
-    </td>
   </tr>
 <?php 
 }
