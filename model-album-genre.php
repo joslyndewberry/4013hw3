f85d8735419e6c53d2e658bd1a024da1675adfd6
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-04 00:18:11 -0500

    Create model-album-genre.php

---
+++ model-album-genre.php
@@ -0,0 +1,16 @@
+<?php
+function selectAlbumGenre() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("select g.genre_id, genre_name, album_name, artist_name, song_name FROM genre g join song s on s.genre_id=g.genre_id join album al on al.album_id = s.album_id join artist a on a.artist_id = al.artist_id");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
