a416c505ff32195e695303ba5649aeb6827c729f
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-03 23:00:16 -0500

    Update model-albums-by-artist.php

--- model-albums-by-artist.php
+++ model-albums-by-artist.php
@@ -1,9 +1,9 @@
 <?php
-function selectAlbumsByArtist($aid) {
+function selectAlbumsByArtist($alid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT album_id, album_name, total_tracks FROM `album` al join artist a on a.artist_id = al.artist_id WHERE al.artist_id=?");
-        $stmt->bind_param("i", $aid);
+        $stmt = $conn->prepare("SELECT al.album_id, album_name, total_tracks FROM `album` al join artist a on a.artist_id = al.artist_id WHERE al.artist_id=?");
+        $stmt->bind_param("i", $alid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
