166b03abd961506adb866094057b6a032b23b9f4
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-04 00:01:48 -0500

    Update model-songs-in-album.php

--- model-songs-in-album.php
+++ model-songs-in-album.php
@@ -2,7 +2,7 @@
 function selectSongsInAlbum($sid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT song_id, s.album_id, song_name, duration FROM `song`s join album al on al.album_id = s.album_id WHERE s.album_id=?");
+        $stmt = $conn->prepare("g.genre_id, genre_name, album_name, song_id, song_name, duration, s.album_id FROM genre g join song s on s.genre_id=g.genre_id join album al on al.album_id = s.album_id join artist a on a.artist_id = al.artist_id WHERE s.album_id=?");
         $stmt->bind_param("i", $sid);
         $stmt->execute();
         $result = $stmt->get_result();
