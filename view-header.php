4535d6c757d0a34e06765a2315a76b6e0cb1274c
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-03 23:26:18 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -31,7 +31,7 @@
           <a class="nav-link" href="songs.php">Songs</a>
         </li>
            <li class="nav-item">
-          <a class="nav-link" href="artist-albums.php">Artist with Albums</a>
+          <a class="nav-link" href="artist-genre.php">Artist Genre</a>
         </li>
       </ul>
     </div>
