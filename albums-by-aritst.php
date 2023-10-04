bbb198306090198a38edce28764debda558387f0
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-03 22:19:31 -0500

    Create albums-by-aritst.php

---
+++ albums-by-aritst.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-albums-by-artist.php");
+
+$pageTitle = "Albums by Artist";
+include "view-header.php";
+$songs = selectAlbumsByArtist($_POST['alid']);
+include "view-albums-by-artist.php";
+include "view-footer.php"
+?>
