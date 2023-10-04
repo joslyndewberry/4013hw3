d5e720146dc9d4dffdcf45e42199e4611a5ca2db
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-04 00:26:28 -0500

    Update model-album-genre.php

--- model-album-genre.php
+++ model-album-genre.php
@@ -2,7 +2,7 @@
 function selectAlbumGenre() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("select g.genre_id, genre_name, album_name, artist_name, total_tracks FROM genre g join song s on s.genre_id=g.genre_id join album al on al.album_id = s.album_id join artist a on a.artist_id = al.artist_id");
+        $stmt = $conn->prepare("select DISTINCT g.genre_id, genre_name, album_name, artist_name, total_tracks FROM genre g join song s on s.genre_id=g.genre_id join album al on al.album_id = s.album_id join artist a on a.artist_id = al.artist_id");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
