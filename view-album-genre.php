f05bbc7b422af0197f868e92dd06e609a41d3336
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-04 00:25:08 -0500

    Update view-album-genre.php

--- view-album-genre.php
+++ view-album-genre.php
@@ -6,7 +6,6 @@
       <th>ID</th>
       <th>Name</th>
       <th>Total Tracks</th>
-        <th>Songs</th>
         <th>Artist</th>
         <th>Genre</th>
       </tr>
@@ -19,7 +18,6 @@ while ($album = $albums->fetch_assoc()) {
     <td><?php echo $album['album_id']; ?></td>
     <td><?php echo $album['album_name']; ?></td>
     <td><?php echo $album['total_tracks']; ?></td>
-    <td><?php echo $album['song_name']; ?></td>
     <td><?php echo $album['artist_name']; ?></td>
    <td><?php echo $album['genre_name']; ?></td>
   </tr>
