bebdb0d91efbe3605c45b4656f7ac695a7fbfdda
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-03 20:21:42 -0500

    Create model-artists.php

---
+++ model-artists.php
@@ -0,0 +1,16 @@
+<?php
+function selectArtists() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT artist_id, artist_name FROM `artist`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
