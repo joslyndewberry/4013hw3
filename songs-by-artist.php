6d316166f81c7090e64ade5e4554daaa4942b92b
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-03 21:37:31 -0500

    Update and rename songs-by-artists.php to songs-by-artist.php

---
+++ songs-by-artist.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-songs-by-artist.php");
+
+$pageTitle = "Songs by Artist";
+include "view-header.php";
+$songs = selectSongsByArtist($_GET['id']);
+include "view-songs-by-artist.php";
+include "view-footer.php"
+?>
