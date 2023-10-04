45926832bd5b26d716d0069ca00fc43bb5e74953
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-03 21:19:34 -0500

    Create view-songs.php

---
+++ view-songs.php
@@ -0,0 +1,16 @@
+<?php
+function selectSongs() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT song_id, album_id, genre_id, song_name FROM `song`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
