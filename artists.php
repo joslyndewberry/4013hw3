f2b39907de00be26091e624833c4b42acf39da5e
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-03 20:17:42 -0500

    Create artists.php

---
+++ artists.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-artists.php");
+
+$pageTitle = "Artists";
+include "view-header.php";
+$artists = selectArtists();
+include "view-artists.php";
+include "view-footer.php"
+?>
