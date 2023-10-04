0c9aa5d2aa8a58fe7e908f56eec1d4742d7f1270
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-04 00:15:07 -0500

    Create album-genre.php

---
+++ album-genre.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-album-genre.php");
+
+$pageTitle = "Album Genre";
+include "view-header.php";
+$albums = selectAlbumGenre();
+include "view-album-genre.php";
+include "view-footer.php"
+?>
