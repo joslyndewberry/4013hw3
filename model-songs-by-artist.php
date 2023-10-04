f6b4e2484921003d63d8a8487fcf09dfd155d40c
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-03 23:58:15 -0500

    Update model-songs-by-artist.php

--- model-songs-by-artist.php
+++ model-songs-by-artist.php
@@ -2,7 +2,7 @@
 function selectSongsByArtist($aid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT song_id, song_name, duration, s.album_id FROM `song` s join album al on al.album_id = s.album_id join artist a on a.artist_id = al.artist_id where al.artist_id=?");
+        $stmt = $conn->prepare("SELECT g.genre_id, genre_name, album_name, song_id, song_name, duration, s.album_id FROM genre g join song s on s.genre_id=g.genre_id join album al on al.album_id = s.album_id join artist a on a.artist_id = al.artist_id where al.artist_id=?");
         $stmt->bind_param("i", $aid);
         $stmt->execute();
         $result = $stmt->get_result();
