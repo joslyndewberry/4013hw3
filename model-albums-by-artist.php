1fe0b8132ff83a15aa21b4d2f180471cc8522c97
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-03 22:24:27 -0500

    Create model-albums-by-artist.php

---
+++ model-albums-by-artist.php
@@ -0,0 +1,17 @@
+<?php
+function selectAlbumsByArtist($alid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT album_id, album_name, total_tracks FROM `album` al join artist a on a.artist_id = al.artist_id WHERE al.artist_id=?");
+        $stmt->bind_param("i", $alid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
