c757ec1a1b75991126bb5fa9df6c7802c5360e9a
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-24 08:49:20 -0500

    Update view-artists-newform.php

--- view-artists-newform.php
+++ view-artists-newform.php
@@ -16,8 +16,8 @@
       <div class="modal-body">
         <form method="post" action="">
           <div class="mb-3">
-            <label for="artist_name" class="form-label">Artist Name</label>
-            <input type="text" class="form-control" id="artist_name" name="artist_name">
+            <label for="aname" class="form-label">Artist Name</label>
+            <input type="text" class="form-control" id="aname" name="aname">
           </div>
            <input type="hidden" name="actionType" value="Add">
           <button type="submit" class="btn btn-primary">Save</button>
