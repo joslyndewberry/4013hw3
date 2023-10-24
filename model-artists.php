608596ef7aef2a80dfb2ffa6f82baa6ea9f7a075
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-24 08:01:40 -0500

    Update model-artists.php

--- model-artists.php
+++ model-artists.php
@@ -13,4 +13,46 @@ function selectArtists() {
     }
 }
 
+function insertArtist($aname) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO 'artist' ('artist_name') VALUES (?)");
+        $stmt->bind_param("s", $aname);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateArtist($aname, $artid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update 'artist' set 'artist_name'=? where artist_id = ?");
+        $stmt->bind_param("si", $aname, $artid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteArtist($artid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from artist where artist_id = ?");
+        $stmt->bind_param("i", $artid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 ?>
