c2ba20b5bde0e0524da9da9fb1ef0155228306e9
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-24 08:39:24 -0500

    Update artists.php

--- artists.php
+++ artists.php
@@ -4,6 +4,16 @@ require_once("model-artists.php");
 
 $pageTitle = "Artists";
 include "view-header.php";
+
+
+if (isset($_POST['actionType'])) {
+  switch($_POST['actionType']) {
+    case "Add":
+      insertArtist($_POST['artname']);
+      break;
+  }
+}
+
 $artists = selectArtists();
 include "view-artists.php";
 include "view-footer.php"
