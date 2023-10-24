cf923f6fb8532c63adc7c022cc2cef4e2e31848b
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-24 08:40:33 -0500

    Update artists.php

--- artists.php
+++ artists.php
@@ -9,7 +9,7 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch($_POST['actionType']) {
     case "Add":
-      insertArtist($_POST['artname']);
+      insertArtist($_POST['aname']);
       break;
   }
 }
