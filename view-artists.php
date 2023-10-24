5c3a505eee91f773b0d2b847b52a5c157802758e
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-24 08:11:12 -0500

    Update view-artists.php

--- view-artists.php
+++ view-artists.php
@@ -3,9 +3,9 @@
     <h1>Artists</h1>
   </div>
   <div class="col-auto">
-    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
-    </svg>
+   <?php
+    Include ="view-artists-newform.php";
+    ?>
   </div>
 </div>
 <div class="table-responsive">
