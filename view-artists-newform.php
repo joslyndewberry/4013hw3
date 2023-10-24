2bcd0f5ce888483746e77d659e3c324026f0d2ef
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-24 08:23:51 -0500

    Create view-artists-newform.php

---
+++ view-artists-newform.php
@@ -0,0 +1,27 @@
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newArtistModal">
+   <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+    </svg>
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="newArtistModal" tabindex="-1" aria-labelledby="newArtistModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="newArtistModalLabel">New Artist</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form method="post" action="">
+          <div class="mb-3">
+            <label for="artist_name" class="form-label">Artist Name</label>
+            <input type="text" class="form-control" id="artist_name" name="artist_name">
+          </div>
+          <button type="submit" class="btn btn-primary">Save</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
