23f4d4f89187a4db0d7cec543abc54375e74ce70
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-24 23:46:11 -0500

    Update albums.php

--- albums.php
+++ albums.php
@@ -4,6 +4,15 @@ require_once("model-albums.php");
 
 $pageTitle = "Albums";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch($_POST['actionType']) {
+    case "Add":
+      insertAlbum($_POST['alName'], $_POST['tracks']);
+      break;
+  }
+}
+
 $albums = selectAlbums();
 include "view-albums.php";
 include "view-footer.php"
