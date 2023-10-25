0bef7c6444716f1ebc8749cd76113b43219ca8f4
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-25 12:43:08 -0500

    Update model-songs.php

--- model-songs.php
+++ model-songs.php
@@ -13,4 +13,46 @@ function selectSongs() {
     }
 }
 
+function insertSong($sName, $length) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `song` (`song_name`, `duration`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $sName, $length);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateArtist($sName, $length, $songid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update 'song' set 'song_name'=?, 'duration'=? where artist_id = ?");
+        $stmt->bind_param("ssi", $sName, $length, $songid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteArtist($songid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from song where song_id = ?");
+        $stmt->bind_param("i", $songid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 ?>
