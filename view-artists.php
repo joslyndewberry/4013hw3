239d2b366835048b89b04e8093795a60e9396528
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-10-25 00:51:34 -0500

    Update view-artists.php

--- view-artists.php
+++ view-artists.php
@@ -15,6 +15,8 @@
       <th>ID</th>
       <th>Name</th>
         <th></th>
+        <th></th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
@@ -25,6 +27,19 @@ while ($artist = $artists->fetch_assoc()) {
     <td><?php echo $artist['artist_id']; ?></td>
     <td><?php echo $artist['artist_name']; ?></td>
     <td><a href="songs-by-artist.php?id=<?php echo $artist['artist_id']; ?>">Songs</a></td>
+    <td>
+    </td>
+    <td>
+      <form method="post" action="">
+        <input type="hidden" name="aid" value="<?php echo $artist['artist_id']; ?>">
+        <input type="hidden" name="actionType" value="Delete">
+        <button type="submit" class="btn btn-primary">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+          </svg>
+        </button>
+      </form>
+    </td>
   </tr>
 <?php 
 }
