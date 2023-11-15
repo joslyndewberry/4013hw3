0705806ce38b64f1c8b5c4637b5a8549035d2fbe
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-11-15 00:01:16 -0600

    Update artists-chart.php

--- model/artists-chart.php
+++ model/artists-chart.php
@@ -3,7 +3,7 @@
 function selectArtists() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT artist_name, count(song_id) as num_songs FROM song s join album al on al.album_id = s.album_id join artist a on a.artist_id = al.artist_id group by artist_name");
+        $stmt = $conn->prepare("SELECT artist_name, count(song_id) as num_songs, count(album_id) as num_albums FROM song s join album al on al.album_id = s.album_id join artist a on a.artist_id = al.artist_id group by artist_name");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
