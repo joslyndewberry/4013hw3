ece88dbb52febf23a767993b3c6922dea1d1741f
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-11-14 19:16:58 -0600

    Update album-genre.php

--- album-genre.php
+++ album-genre.php
@@ -3,8 +3,8 @@ require_once("util-db.php");
 require_once("model/album-genre.php");
 
 $pageTitle = "Album Genre";
-include "view-header.php";
+include "view/header.php";
 $albums = selectAlbumGenre();
-include "view-album-genre.php";
-include "view-footer.php"
+include "view/album-genre/page.php";
+include "view/footer.php"
 ?>
