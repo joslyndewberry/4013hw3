31a348bf8a0fc3f8f6f15869aec3a68e7812e052
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-11-14 20:17:53 -0600

    Update artists-chart.php

--- model/artists-chart.php
+++ model/artists-chart.php
@@ -3,7 +3,7 @@
 function selectArtists() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT artist_id, artist_name FROM `artist`");
+        $stmt = $conn->prepare("SELECT g.genre_id, genre_name, album_name, song_id, song_name, duration, s.album_id FROM genre g join song s on s.genre_id=g.genre_id join album al on al.album_id = s.album_id join artist a on a.artist_id = al.artist_id where al.artist_id=?");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
