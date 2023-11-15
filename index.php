e65c176691cab35e537e6e3b81fb3f17092b3fa6
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-11-14 19:24:39 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 $pageTitle = "Home";
-include "view-header.php";
+include "view/header.php";
 ?>
       
     <h1>Homework 3</h1>
 
 <?php
-include "view-footer.php"
+include "view/footer.php"
 ?>
