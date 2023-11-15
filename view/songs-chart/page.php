7fce88c812ff7fbe146a26581958b7d6d77f9d8f
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-11-14 23:44:18 -0600

    Update page.php

--- view/songs-chart/page.php
+++ view/songs-chart/page.php
@@ -9,38 +9,28 @@
 <script>
   const ctx = document.getElementById('myChart');
 
-    $labels = ['Label 1', 'Label 2', 'Label 3', 'Label 4', 'Label 5'];
-    $data = [12, 19, 3, 5, 2];
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+    datasets: [{
+        data: [
+<?php
+while ($artist = $artists->fetch_assoc()) {
+  echo $artist['num_songs'] . ", ";
+}
+?>
+          ]
+    }],
 
-    $labels_json = json_encode($labels);
-    $data_json = json_encode($data);
-    ?>
-
-    <script>
-        var data = {
-            labels: <?php echo $labels_json; ?>,
-            datasets: [{
-                label: 'Sample Bar Chart',
-                data: <?php echo $data_json; ?>,
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
-            }]
-        };
-
-        var options = {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        };
-
-        var ctx = document.getElementById('myBarChart').getContext('2d');
-
-        var myBarChart = new Chart(ctx, {
-            type: 'bar',
-            data: data,
-            options: options
-        });
-    </script>
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+     <?php
+      $artists = selectArtists();
+        while ($artist = $artists->fetch_assoc()) {
+          echo "'" . $artist['artist_name'] . "', ";
+        }
+      ?>
+    ]
+},
+});
+</script>
