c3960c719513c9427aed6a75971728198b6a9569
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-11-15 00:32:50 -0600

    Create page.php

---
+++ view/genres-chart/page.php
@@ -0,0 +1,20 @@
+options: {
+    indexAxis: 'y',
+    // Elements options apply to all of the options unless overridden in a dataset
+    // In this case, we are setting the border of each horizontal bar to be 2px wide
+    elements: {
+      bar: {
+        borderWidth: 2,
+      }
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'right',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Horizontal Bar Chart'
+      }
+    }
+  },
