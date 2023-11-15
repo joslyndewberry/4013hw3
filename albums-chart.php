21fe74169e8659fa917c386033bc4be72cd5a9f5
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-11-15 00:11:23 -0600

    Update albums-chart.php

--- albums-chart.php
+++ albums-chart.php
@@ -1,10 +1,10 @@
 <?php
 require_once("util-db.php");
-require_once("model/artists-chart.php");
+require_once("model/albums-chart.php");
 
 $pageTitle = "Albums Chart";
 include "view/header.php";
-$artists = selectArtists();
+$albums = selectAlbums();
 include "view/albums-chart/page.php";
 include "view/footer.php"
 ?>
