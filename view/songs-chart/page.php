14264372c6e569d6e85fef0c94676925f444ad23
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-11-14 23:23:01 -0600

    Update page.php

--- view/songs-chart/page.php
+++ view/songs-chart/page.php
@@ -25,13 +25,7 @@
         barThickness: 6,
         maxBarThickness: 8,
         minBarLength: 2,
-        data: [
-          <?php
-                while ($artist = $artists->fetch_assoc()) {
-                echo $artist['num_songs'] . ", ";
-              }
-          ?>
-        ]
+        data: [10, 20, 30, 40, 50, 60, 70]
     }]
 },
 });
