9db046ff82800ac787427e4183db30c48aa2fe92
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-11-14 22:28:27 -0600

    Update page.php

--- view/artists-chart/page.php
+++ view/artists-chart/page.php
@@ -2,6 +2,7 @@
 <h1>Artists Charts</h1>
 
  <div class = "row"> 
+  <div>
 <div>
   <canvas id="myChart"></canvas>
 </div>
@@ -35,4 +36,54 @@ while ($artist = $artists->fetch_assoc()) {
 },
 });
 </script>
+  </div>
+
+<?php
+while ($artist = $artists->fetch_assoc()) {
+  echo $artist['num_songs'] . ", ";
+}
+?>
+  
+<div>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+    datasets: [{
+        barPercentage: 0.5,
+        barThickness: 6,
+        maxBarThickness: 8,
+        minBarLength: 2,
+        data: [
+          <?php
+             while ($artist = $artists->fetch_assoc()) {
+             echo $artist['num_songs'] . ", ";
+            }
+          ?> 
+        ]
+    }]
+};
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+     <?php
+      $artists = selectArtists();
+        while ($artist = $artists->fetch_assoc()) {
+          echo "'" . $artist['artist_name'] . "', ";
+        }
+      ?>
+    ]
+},
+});
+</script>
+  </div>
+  
 </div>
