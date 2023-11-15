b725062b93fd5a6ae9f3f2245072b4962a96783a
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-11-14 23:59:02 -0600

    Create page.php

---
+++ view/albums-chart/page.php
@@ -0,0 +1,41 @@
+
+<h1>Albums Chart</h1>
+
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+    datasets: [{
+        barPercentage: 0.5,
+        barThickness: 100,
+        maxBarThickness: 100,
+        minBarLength: 2,
+        label: 'Artists',
+        data: [
+<?php
+while ($artist = $artists->fetch_assoc()) {
+  echo $artist['num_songs'] . ", ";
+}
+?>
+          ]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+     <?php
+      $artists = selectArtists();
+        while ($artist = $artists->fetch_assoc()) {
+          echo "'" . $artist['artist_name'] . "', ";
+        }
+      ?>
+    ]
+},
+});
+</script>
