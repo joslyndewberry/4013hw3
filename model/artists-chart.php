f2c7a17635b285972bf853b0994e5276a8c83f20
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-11-14 20:13:43 -0600

    Update artists-chart.php

--- model/artists-chart.php
+++ model/artists-chart.php
@@ -1,1 +1,17 @@
 
+<?php
+function selectArtists() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT artist_id, artist_name FROM `artist`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
