82ebd71a454ff0c37d4359fc1801b371b5d773ae
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-11-15 00:41:24 -0600

    Update page.php

--- view/genres-chart/page.php
+++ view/genres-chart/page.php
@@ -1,3 +1,36 @@
+<h1>Genres Chart</h1>
+
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+    datasets: [{
+        data: [
+<?php
+while ($genre = $genres->fetch_assoc()) {
+  echo $genre['num_songs'] . ", ";
+}
+?>
+          ]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+     <?php
+      $genres = selectGenres();
+        while ($genre = $genres->fetch_assoc()) {
+          echo "'" . $genre['genre_name'] . "', ";
+        }
+      ?>
+    ]
+},
 options: {
     indexAxis: 'y',
     // Elements options apply to all of the options unless overridden in a dataset
@@ -18,3 +51,5 @@ options: {
       }
     }
   },
+});
+</script>
