628c8a881addd62b1414042f6a2b8caa46514e65
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-11-14 19:20:18 -0600

    Update songs.php

--- songs.php
+++ songs.php
@@ -1,10 +1,10 @@
 <?php
 require_once("util-db.php");
-require_once("model-songs.php");
+require_once("model/songs.php");
 
 $pageTitle = "Songs";
-include "view-header.php";
+include "view/header.php";
 $songs = selectSongs();
-include "view-songs.php";
-include "view-footer.php"
+include "view/songs/page.php";
+include "view/footer.php"
 ?>
